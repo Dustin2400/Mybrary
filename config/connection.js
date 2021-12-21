7cf46b7f9c2495eb024e48a39fc6d7f5afaa7baa
trujilml <mickeytrj@gmail.com>
2021-12-20 21:07:33 -0600

    completed connection js file for now - will update when needed - mickey/trujilml

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//insert sequelize database stuff
+//entered sequelize format for connection.js - will need to see up personal .env file later on prior to launching the server
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
