403ba26bf48fd9b0f53022e16a3d2a44b00844df
trujilml <mickeytrj@gmail.com>
2021-12-21 19:09:52 -0600

    completed server js file - modify if needed - mickey/trujilml

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connection-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Top secret project',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Listening...'));
+});
