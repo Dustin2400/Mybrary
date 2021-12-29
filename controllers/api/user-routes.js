f9027b43c1a5daca616e13a93fc85bb9aca108b9
trujilml <mickeytrj@gmail.com>
2021-12-28 22:50:47 -0600

    worked on multiple handlebar files, fixed an error with signup page still not visible on server site - mickey

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,13 +51,13 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
     .then(dbUserData => {
-        // req.session.save(() => {
-        //     req.session.user_id = dbUserData.id;
-        //     req.session.username = dbUserData.username;
-        //     req.session.loggedIn = true;
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
             res.json(dbUserData);
-        // });
+        });
     })
     .catch(err => {
         console.log(err);
